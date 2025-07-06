b91ffa4862b46e4e37414ee36d759963675b9470
chickenONsteak <austinchenwd@gmail.com>
2025-07-06 10:52:18 +0800

    complete

--- app.js
+++ app.js
@@ -397,10 +397,54 @@ let totalYearsLived = 0;
 // Complete the exercise in the space below:
 totalYearsLived = inventors.reduce((sumTotal, obj) => {
   const yearsLived = obj.passed - obj.year;
-  const total = sumTotal + yearsLived;
-  return total;
-});
+  return (sumTotal += yearsLived);
+}, totalYearsLived);
 
 // Check your work:
 console.log("Level Up 1 my result: ", totalYearsLived);
 console.log("Level Up 1 correct result: ", 861);
+
+/*
+Level Up exercise 2: Array.prototype.reduce()
+
+Tallying travel methods using Array.prototype.reduce(). 
+
+Count the number of times each travel method appears in the 'travelMethods'
+array.
+
+- The resulting object should have keys as the travel methods 
+  ('car', 'truck', 'bike', etc.) and values as their respective counts.
+- Store this object in the variable 'travelMethodCounts'.
+
+Hints:
+- Inside the reduce function, check if the travel method already exists as a key
+  in your accumulator object. If it does, increment its count. If not, add it 
+  to the object and give it a value of 1.
+- Since you want to return an object, be sure to pass an empty {} for the 
+  initial value of the "accumulator".
+*/
+
+let travelMethodCounts = {};
+
+// Complete the exercise in the space below:
+travelMethodCounts = travelMethods.reduce((accumulator, travelMethod) => {
+  // accumulator is an empty object initialised for this reduce method
+  if (accumulator[travelMethod]) {
+    // if accumulator does not have a key with the variable travelMethod (e.g. car, truck, bike), accumulator[travelMethod] returns undefined -> falsey value
+    accumulator[travelMethod]++; // add count for the value of accumulator[travelMethod]
+  } else {
+    // initialise that KVP within the travelMethodCounts object
+    accumulator[travelMethod] = 1;
+  }
+  return accumulator;
+}, {});
+
+// Check your work:
+console.log("Level Up 2 my result: ", travelMethodCounts);
+console.log("Level Up 2 correct result: ", {
+  car: 5,
+  truck: 3,
+  bike: 2,
+  walk: 2,
+  van: 2,
+});
